21860fc679182d761d78c7fd175023505a586d76
Tommy Tong <cktong02@gmail.com>
2019-07-18 23:06:32 +0800

    init place order test

---
+++ test/place.spec.js
@@ -0,0 +1,7 @@
+describe("Order Endpoints", function () {
+    describe("Place Order", function () {
+        describe("When stops are invalid", function () {
+            it("should display missing stops error");
+        });
+    });
+});
