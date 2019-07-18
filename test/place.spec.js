345524208d5020ae58fe2ef7d71b008c6ec83d62
Tommy Tong <cktong02@gmail.com>
2019-07-18 23:08:07 +0800

    extra test scenarios

--- test/place.spec.js
+++ test/place.spec.js
@@ -3,5 +3,21 @@ describe("Order Endpoints", function () {
         describe("When stops are invalid", function () {
             it("should display missing stops error");
         });
+
+        describe("When an order is placed at daytime", function () {
+            it("should return new order with standard fare");
+        });
+
+        describe("When an order is placed at late night", function () {
+            it("should return new order with extra fare");
+        });
+
+        describe("When an advance order is placed", function () {
+            it("should return new order");
+        });
+
+        describe("When a past order is placed", function () {
+            it("should display error");
+        });
     });
 });
